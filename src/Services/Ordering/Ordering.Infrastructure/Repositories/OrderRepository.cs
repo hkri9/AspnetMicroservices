4ab487a78cf3948a7fe1c89ce8a082efb8f9f831
Kristina Volkov <hkri9@yahoo.com>
2021-07-08 15:50:43 -0500

    configurations added to API

---
+++ src/Services/Ordering/Ordering.Infrastructure/Repositories/OrderRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Contracts.Persistence;
+using Ordering.Domain.Entities;
+using Ordering.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Repositories
+{
+    public class OrderRepository : RepositoryBase<Order>, IOrderRepository
+    {
+
+        public OrderRepository(OrderContext dbcontext) :base (dbcontext)
+        {
+
+        }
+
+        public async Task<IEnumerable<Order>> GetOrdersByUserName(string userName)
+        {
+            //same return await GetAsync(o => o.UserName == userName);
+
+            var orderList = await _dbContext.Orders
+                .Where(o => o.UserName == userName)
+                .ToListAsync();
+
+            return orderList;
+        }
+
+        
+    }
+}
